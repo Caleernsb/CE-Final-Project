31529b2e6049b937450c3d5e8d2b1f02cabaa8a3
Caleernsb <167924878+Caleernsb@users.noreply.github.com>
2024-04-23 20:13:29 -0400

    Update <caleernsberger>_Final.py

--- <caleernsberger>_Final.py
+++ <caleernsberger>_Final.py
@@ -84,7 +84,7 @@ class Game:
         self.screen_width = 800
         self.screen_height = 600
         self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        pygame.display.set_caption("Block Game")
+        pygame.display.set_caption("coin-dash/dodge")
 
         # Load background image
         self.background_image = pygame.image.load("sky2.jpg").convert()
